cddde632163295875fd067e54f9d4dbe9f66ab2d
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-24 15:31:29 +0000

    Node js start

---
+++ chalk.mjs
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk)
+console.log(chalk.blue('Hello, world!'));
+console.log(chalk.red.bold('This is a bold red message'));
+console.log(chalk.green.underline('Green and underlined!'));
+console.log(chalk.bgYellow.black('Black text with yellow background'));
